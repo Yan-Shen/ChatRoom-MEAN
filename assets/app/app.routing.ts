6b7199a6bc27968351cc8aa7dd657cc653de5e61
Yan.Shen <sheny308@yahoo.com>
2018-03-06 11:42:12 -0500

    add

--- assets/app/app.routing.ts
+++ assets/app/app.routing.ts
@@ -6,6 +6,7 @@ import { AuthenticationComponent } from "./auth/authentication.component";
 const APP_ROUTES: Routes = [
     { path: '', redirectTo: '/messages', pathMatch: 'full' },
     { path: 'messages', component: MessagesComponent },
+    // loadChildren: enable lazy loading.  only render components when it is needed
     { path: 'auth', component: AuthenticationComponent, loadChildren: './auth/auth.module#AuthModule' }
 ];
 
