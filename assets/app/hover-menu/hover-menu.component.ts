1f1c53e4b936771b358acd154c061699ab327ba6
Yan.Shen <sheny308@yahoo.com>
2018-03-29 14:04:58 -0400

    add hover menu

---
+++ assets/app/hover-menu/hover-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hover-menu',
+  templateUrl: './hover-menu.component.html',
+  styleUrls: ['./hover-menu.component.css']
+})
+export class HoverMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
