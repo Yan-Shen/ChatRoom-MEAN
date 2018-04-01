48d75c5255da34b2f1e871156cba4f0349b6d8bb
Yan.Shen <sheny308@yahoo.com>
2018-03-31 23:21:37 -0400

    sidepannel

--- assets/app/hover-menu/hover-menu.component.ts
+++ assets/app/hover-menu/hover-menu.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-hover-menu',
   templateUrl: './hover-menu.component.html',
   styleUrls: ['./hover-menu.component.css']
 })
-export class HoverMenuComponent implements OnInit {
+
+export class HoverMenuComponent{
+  @Output() editClicked = new EventEmitter<void>();
+  @Output() deleteClicked = new EventEmitter<void>();
 
   constructor() { }
 
-  ngOnInit() {
+  onEdit() {
+    console.log('child edit called---')
+    this.editClicked.emit();
+  }
+
+  onDelete() {
+    console.log('child delete called---')
+    this.deleteClicked.emit();
   }
 
 }
